9e4e4ea40b7c8f4b9aa45e8d5c111c2a51ee7894
andresarmento <diybrasil@gmail.com>
2015-02-22 12:19:14 -0300

    MBAP compliant response and checks

--- libraries/ModbusIP/ModbusIP.cpp
+++ libraries/ModbusIP/ModbusIP.cpp
@@ -24,14 +24,16 @@ void ModbusIP::proc() {
         if (client.connected()) {
             int i = 0;
             while (client.available()){
-                MBAP[i] = client.read();
+                _MBAP[i] = client.read();
                 i++;
                 if (i==7) break;  //MBAP length has 7 bytes size
             }
-			_len = MBAP[4] << 8 | MBAP[5];
+			_len = _MBAP[4] << 8 | _MBAP[5];
 			_len--;  // Do not count with last byte from MBAP
 
-			if (_len > MODBUSIP_MAXFRAME) return;
+			if (_MBAP[2] !=0 | _MBAP[3] !=0) return;   //Not a MODBUSIP packet
+			if (_len > MODBUSIP_MAXFRAME) return;      //Length is over MODBUSIP_MAXFRAME
+
 			_frame = (byte*) malloc(_len);
 			i = 0;
 			while (client.available()){
@@ -40,10 +42,14 @@ void ModbusIP::proc() {
                 if (i==_len) break;
 			}
 
-			if (this->receivePDU(_frame)) {
+			if (this->receivePDU(_frame) && _reply != REPLY_OFF)  {
+                //MBAP
+                _MBAP[4] = _len >> 8;
+                _MBAP[5] = _len | 0x00FF;
 				for (i = 0 ; i < 7 ; i++) {
-					client.write((byte)0);
+					client.write(_MBAP[i]);
 				}
+                //PDU Frame
 				for (i = 0 ; i < _len ; i++) {
 					client.write(_frame[i]);
 				}
