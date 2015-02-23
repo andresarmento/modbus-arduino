d86bb61eac889445107983cb78df0983f41bdfb1
andresarmento <diybrasil@gmail.com>
2015-02-23 14:43:09 -0300

    initial commit - switch examples

---
+++ libraries/ModbusIP/examples/Switch/Switch.ino
@@ -0,0 +1,39 @@
+/*
+  Modbus-Arduino Example - Switch (Modbus IP)
+  Copyright by André Sarmento Barbosa
+  http://github.com/andresarmento/modbus-arduino
+*/
+ 
+#include <SPI.h>
+#include <Ethernet.h>
+#include <Modbus.h>
+#include <ModbusIP.h>
+
+//Modbus Registers Offsets (0-9999)
+const int SWITCH_ISTS = 100; 
+//Used Pins
+const int switchPin = 3;
+
+//ModbusIP object
+ModbusIP mb;
+
+void setup() {
+    // The media access control (ethernet hardware) address for the shield
+    byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };  
+    // The IP address for the shield
+    byte ip[] = { 192, 168, 1, 120 };   
+    //Config Modbus IP 
+    mb.config(mac, ip);
+    //Set ledPin mode
+    pinMode(switchPin, INPUT);
+    // Add SWITCH_ISTS register - Use addIsts() for digital inputs 
+    mb.addIsts(SWITCH_ISTS);
+}
+
+void loop() {
+   //Call once inside loop() - all magic here
+   mb.task();
+   
+   //Attach switchPin to SWITCH_ISTS register     
+   mb.Ists(SWITCH_ISTS, digitalRead(SWITCH_ISTS));
+}
---
+++ libraries/ModbusSerial/examples/Switch/Switch.ino
@@ -0,0 +1,36 @@
+/*
+  Modbus-Arduino Example - Switch (Modbus Serial)
+  Copyright by André Sarmento Barbosa
+  http://github.com/andresarmento/modbus-arduino
+*/
+ 
+#include <Modbus.h>
+#include <ModbusSerial.h>
+
+//Modbus Registers Offsets (0-9999)
+const int SWITCH_ISTS = 100; 
+//Used Pins
+const int switchPin = 3;
+
+//ModbusSerial object
+ModbusSerial mb;
+
+void setup() {
+    // Config Modbus Serial (port, speed, byte format) 
+    mb.config(&Serial, 38400, SERIAL_8N1);
+    // Set the Slave ID (1-247)
+    mb.setSlaveId(10);  
+
+    //Set ledPin mode
+    pinMode(switchPin, INPUT);
+    // Add SWITCH_ISTS register - Use addIsts() for digital inputs 
+    mb.addIsts(SWITCH_ISTS);
+}
+
+void loop() {
+   //Call once inside loop() - all magic here
+   mb.task();
+   
+   //Attach switchPin to SWITCH_ISTS register     
+   mb.Ists(SWITCH_ISTS, digitalRead(SWITCH_ISTS));
+}
