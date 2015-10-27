c726a872cffd1034b8e4739523ebe25a9c99abfc
andre <diybrasil@gmail.com>
2015-10-27 15:14:38 -0200

    SoftwareSerial support (untested)

--- libraries/ModbusSerial/ModbusSerial.cpp
+++ libraries/ModbusSerial/ModbusSerial.cpp
@@ -40,6 +40,31 @@ bool ModbusSerial::config(HardwareSerial* port, long baud, u_int format, int txP
     return true;
 }
 
+#ifdef USE_SOFTWARE_SERIAL
+bool ModbusSerial::config(SoftwareSerial* port, long baud, int txPin) {
+    this->_port = port;
+    this->_txPin = txPin;
+    (*port).begin(baud);
+
+    delay(2000);
+
+    if (txPin >= 0) {
+        pinMode(txPin, OUTPUT);
+        digitalWrite(txPin, LOW);
+    }
+
+    if (baud > 19200) {
+        _t15 = 750;
+        _t35 = 1750;
+    } else {
+        _t15 = 15000000/baud; // 1T * 1.5 = T1.5
+        _t35 = 35000000/baud; // 1T * 3.5 = T3.5
+    }
+
+    return true;
+}
+#endif
+
 #ifdef __AVR_ATmega32U4__
 bool ModbusSerial::config(Serial_* port, long baud, u_int format, int txPin) {
     this->_port = port;
--- libraries/ModbusSerial/ModbusSerial.h
+++ libraries/ModbusSerial/ModbusSerial.h
@@ -8,6 +8,12 @@
 #ifndef MODBUSSERIAL_H
 #define MODBUSSERIAL_H
 
+//#define USE_SOFTWARE_SERIAL
+
+#ifdef USE_SOFTWARE_SERIAL
+#include <SoftwareSerial.h>
+#endif
+
 class ModbusSerial : public Modbus {
     private:
         Stream* _port;
@@ -22,10 +28,13 @@ class ModbusSerial : public Modbus {
         ModbusSerial();
         bool setSlaveId(byte slaveId);
         byte getSlaveId();
+        bool config(HardwareSerial* port, long baud, u_int format, int txPin=-1);
+        #ifdef USE_SOFTWARE_SERIAL
+        bool config(SoftwareSerial* port, long baud, int txPin=-1);
+        #endif
         #ifdef __AVR_ATmega32U4__
         bool config(Serial_* port, long baud, u_int format, int txPin=-1);
         #endif
-        bool config(HardwareSerial* port, long baud, u_int format, int txPin=-1);
         void task();
         bool receive(byte* frame);
         bool sendPDU(byte* pduframe);
